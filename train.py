7ec21c615d4fdfbcafef1c6bed513a8339d9bf43
whatsnextthen <88406171+whatsnextthen@users.noreply.github.com>
2022-09-11 22:09:01 +0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,58 @@
+import re
+
+d = {}
+cnt = {}
+prob = {}
+
+def tokensize_text(text):
+    t = re.sub(r'[^\w\s]', '', text.lower())
+    new_t = t.split(' ')
+    return new_t
+
+def training(word):
+    for i in range (0, len(word) - 1):
+        if (word[i] not in d.keys()):
+            d[word[i]] = []
+        ind = len(d[word[i]]) - 1
+        if (word[i + 1] not in d[word[i]]):
+            if (len(d[word[i]]) == 0):
+                cnt[word[i]] = {}
+                cnt[word[i]][word[i + 1]] = 0
+            if (word[i + 1] not in d[word[i]]):
+                d[word[i]].append(word[i + 1])
+            if word[i + 1] not in cnt[word[i]].keys():
+                cnt[word[i]][word[i + 1]] = 0
+            cnt[word[i]][word[i + 1]] += 1
+        else:
+            if word[i + 1] not in cnt[word[i]].keys():
+                cnt[word[i]][word[i + 1]] = 0
+            else:
+                cnt[word[i]][word[i + 1]] += 1
+
+def probability():
+    for k in d.keys():
+        for i in range (len(d[k])):
+            if (k not in prob.keys()):
+                prob[k] = []
+            tot = len(d[k])
+            word = d[k][i]
+            has = cnt[k][word]
+            P = has / tot
+            prob[k].append((word, P))
+
+with open('kaiftext.txt') as f:
+    text = f.read()
+text = tokensize_text(text)
+training(text)
+
+with open('kaiftext.txt') as f:
+    text = f.read()
+text = tokensize_text(text)
+training(text)
+with open('kaiftext.txt') as f:
+    text = f.read()
+text = tokensize_text(text)
+training(text)
+
+probability()
+
